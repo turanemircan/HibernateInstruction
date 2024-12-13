ab5878a2ce4dc6b43a549961cf42c175c8fa1b5c
Emircan <emircanturan010@gmail.com>
2024-12-14 00:04:30 +0300

    Day 05 / many to one / one to many

--- src/main/java/com/tpe/hb06/onetomany/RunnerFetch06.java
+++ src/main/java/com/tpe/hb06/onetomany/RunnerFetch06.java
@@ -5,6 +5,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RunnerFetch06 {
     public static void main(String[] args) {
 
@@ -31,5 +34,18 @@ public class RunnerFetch06 {
         // Ben öğrenci bilgisini sorguladıgım zaman sadece ogrenci bilgileri sorgulanırken
         // ogrencinin sahip oldugu kitapları getir dedigim zaman bizim icin 2. bir sorgu olusturarak kitap bilgilerine erisildi
 
+        // name:'Jack' olan ogrencinin kıtaplarını getirelim
+        // hem student name hemde book bilgilerini ulaşabilmem lazım
+        String sql = "select b.id,b.name from t_book b  join t_student06_t_book sb on sb.booklist_id=b.id join t_student06 s on s.id=sb.student06_id where s.std_name='Jack'";
+        List<Object[]> resultList = session.createSQLQuery(sql).getResultList();
+        for (Object[] s : resultList
+        ) {
+            System.out.println(Arrays.toString(s));
+        }
+        String sql2 = "select b.id,b.name from t_book b inner join t_student06 s on b.std_id=s.id where 'Jack'";
+
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
     }
 }
